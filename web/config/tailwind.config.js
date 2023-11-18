1367610c434108c780629ef77ca61530a25c11c0
Amy Haywood Dutton <me@amyhaywood.com>
2023-11-18 12:28:01 -0600

    💄 Added Tailwind config

--- web/config/tailwind.config.js
+++ web/config/tailwind.config.js
@@ -2,8 +2,27 @@
 module.exports = {
   content: ['src/**/*.{js,jsx,ts,tsx}'],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        supernova: '#fac900' /* yellow / warning */,
+        orangeRed: '#ff4600' /* red / error */,
+        silverTree: '#70bd91' /* light green */,
+        spanishGreen: '#008a52' /* medium green */,
+        cruseo: '#0a5d2c' /* dark green */,
+        countyGreen: '#003d19' /* darkest green */,
+        acadia: '#392f2d' /* brown */,
+        scotchMist: '#efe9cb' /* light brown */,
+        nileBlue: '#243853' /* blue */,
+        blackPearl: '#071126' /* navy blue */,
+        pastelMagenta: '#ff9dbf' /* pink */,
+      },
+    },
+    fontFamily: {
+      condensed: ['Bebas Neue', 'sans-serif'],
+      handwriting: ['Kalam', 'cursive'],
+      sans: ['Inter', 'sans-serif'],
+      script: ['Agbalumo', 'cursive'],
+    },
   },
   plugins: [],
 }
-
